8b08fab4e6a4f007e196c9485699d64fd1477a16
ae <tgioer@163.com>
2018-09-07 18:28:58 +0800

    更新测试数据

--- mysql_dtx_data.sql
+++ mysql_dtx_data.sql
@@ -10,10 +10,26 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-08-31 15:43:43
+Date: 2018-09-07 18:28:20
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for ac_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `ac_permission`;
+CREATE TABLE `ac_permission` (
+  `id` int(11) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `rid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of ac_permission
+-- ----------------------------
+INSERT INTO `ac_permission` VALUES ('1', 'user:add', '用户添加', '1');
 
 -- ----------------------------
 -- Table structure for ac_role
@@ -30,16 +46,31 @@ CREATE TABLE `ac_role` (
 INSERT INTO `ac_role` VALUES ('1', 'admin_role');
 
 -- ----------------------------
+-- Table structure for org_department
+-- ----------------------------
+DROP TABLE IF EXISTS `org_department`;
+CREATE TABLE `org_department` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of org_department
+-- ----------------------------
+INSERT INTO `org_department` VALUES ('1', '软件研发部');
+
+-- ----------------------------
 -- Table structure for org_user
 -- ----------------------------
 DROP TABLE IF EXISTS `org_user`;
 CREATE TABLE `org_user` (
   `uid` int(11) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL
+  `age` int(11) DEFAULT NULL,
+  `dept_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of org_user
 -- ----------------------------
-INSERT INTO `org_user` VALUES ('1', 'ae', '18');
+INSERT INTO `org_user` VALUES ('1', 'ae', '18', '1');
